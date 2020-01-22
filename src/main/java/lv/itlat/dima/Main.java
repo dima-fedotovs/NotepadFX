fb9cbd5c43eb3c5ab4106327f648a39897cd3c63
Dimitrijs Fedotovs <dima@fedoto.ws>
2020-01-22 18:24:20 +0200

    another way to load main.fxml

--- src/main/java/lv/itlat/dima/Main.java
+++ src/main/java/lv/itlat/dima/Main.java
@@ -8,12 +8,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    public static Stage primaryStage;
-
     public void start(Stage stage) throws Exception {
-        Main.primaryStage = stage;
-        Parent root = FXMLLoader.load(
-                getClass().getResource("main.fxml"));
+        var root = new MainController();
         stage.setTitle("Notepad");
         var scene = new Scene(root);
         stage.setScene(scene);
